ea0db67e511db4e7737d900b152f567a5e117e19
yasubeh <mountainsaki@gmail.com>
2017-08-07 03:53:43 +0900

    13-2 Display microposts

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,6 +14,8 @@ User.create!(name: "Example User",
              activated: true,
              activated_at: Time.zone.now)
 
+Faker::Config.locale = :ja
+
 99.times do |n|
   name = Faker::Name.name
   email = "esample-#{n+1}@railstutorial.org"
@@ -24,4 +26,10 @@ User.create!(name: "Example User",
                password_confirmation: password,
                activated: true,
                activated_at: Time.zone.now)
+end
+
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.microposts.create!(content: content) }
 end
