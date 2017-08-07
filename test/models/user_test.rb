ded5a1f68dc007f994422806e2ce5b005bcecf58
yasubeh <mountainsaki@gmail.com>
2017-08-08 04:24:20 +0900

    14-1 Finish relationship model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -78,6 +78,8 @@ class UserTest < ActiveSupport::TestCase
     assert_not michael.following?(archer)
     michael.follow(archer)
     assert michael.following?(archer)
+    #assert archer.followers.include?(michael)
+    assert archer.followed_by?(michael)
     michael.unfollow(archer)
     assert_not michael.following?(archer)
   end
