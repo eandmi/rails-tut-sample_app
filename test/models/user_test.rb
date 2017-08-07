b3efcf9211ff8bb5c332a8cc6af536cbc19d0d84
yasubeh <mountainsaki@gmail.com>
2017-08-08 08:27:38 +0900

    14-3-2 A first feed implementation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -83,4 +83,25 @@ class UserTest < ActiveSupport::TestCase
     michael.unfollow(archer)
     assert_not michael.following?(archer)
   end
+
+  test "feed should have the right posts" do
+    michael = users(:michael)
+    archer  = users(:archer)
+    lana    = users(:lana)
+
+    # confirm microposts by following users
+    lana.microposts.each do |post_following|
+      assert michael.feed.include?(post_following)
+    end
+
+    # confirm microposts by myself 
+    michael.microposts.each do |post_self|
+      assert michael.feed.include?(post_self)
+    end
+
+    # confirm microposts by unfollowed users 
+    archer.microposts.each do |post_unfollowed|
+      assert_not michael.feed.include?(post_unfollowed)
+    end
+  end
 end
